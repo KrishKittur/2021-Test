8152947fc6a1b681d1e4ed693d48861136dafbc6
KrishKittur <73321517+KrishKittur@users.noreply.github.com>
2021-03-02 15:29:08 -0800

    .

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,8 +1,5 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -13,10 +10,8 @@ public class Robot extends TimedRobot {
 
   // Variables
   private final XboxController controller = new XboxController(0);
-  private final CANSparkMax motor1 = new CANSparkMax(22, MotorType.kBrushless);
-  private final CANSparkMax motor2 = new CANSparkMax(23, MotorType.kBrushless);
-  private final CANSparkMax motor3 = new CANSparkMax(25, MotorType.kBrushless);
-  private final CANSparkMax motor4 = new CANSparkMax(26, MotorType.kBrushless);
+  private final CANSparkMax motor1 = new CANSparkMax(25, MotorType.kBrushless);
+  private final CANSparkMax motor2 = new CANSparkMax(26, MotorType.kBrushless);
 
   // Teleop Init
   @Override
